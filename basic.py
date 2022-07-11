ca9515f8645027de11c83fae50cbd36f7c1a492b
Aube <43349927+Aube33@users.noreply.github.com>
2022-07-11 20:57:29 +0200

    Update basic.py

--- basic.py
+++ basic.py
@@ -6,22 +6,7 @@ LINES=50
 
 textures={1:[(70,220,60),"LAND"], 0:[(60,165,220),"WATER"]}
 
-
 matrice=[[random.randint(0,1) for l in range(COLUMNS)] for c in range(LINES)]
-'''
-matrice=[]
-for c in range(LINES):
-    matriceSub=[]
-    for l in range(COLUMNS):
-        matriceSub.append(random.randint(0,1))
-    matrice.append(matriceSub)
-'''
-'''
-for x in matrice:
-    print(x)
-'''   
-
-
 
 
 img=Image.new(mode="RGB", size=(LINES, COLUMNS))
